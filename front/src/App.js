d5cae71ab179af061792fd181e6d046e5dc61a38
Jerrick <jerrickzhu@berkeley.edu>
2021-04-17 16:51:58 -0700

    cleaned up code

--- front/src/App.js
+++ front/src/App.js
@@ -46,7 +46,7 @@ const App = () => {
           img: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>',
           onClick: async () => {
             const dataTest = await annotManager.exportAnnotations( {widgets: false})
-            axios.post("http://localhost:4000", {manualSavedData: dataTest})
+            axios.post("http://localhost:4000/data", {manualSavedData: dataTest})
             .then(async response => {
                 console.log(response);
               })
